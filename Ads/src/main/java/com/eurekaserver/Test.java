63ade2c594656887aa7fba35a9a1f82a86b9b1d1
AmaRHacker007 <vimalhabib1994@gmail.com>
2020-05-31 16:09:29 +0530

    test

---
+++ Ads/src/main/java/com/eurekaserver/Test.java
@@ -0,0 +1,14 @@
+package com.eurekaserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+	
+	 @RequestMapping("/")
+	  public String index() {
+	    return "test.html";
+	  }
+}
